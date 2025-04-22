df352bda70003d3d8eab1396b9124fc9110f5674
tomik0072002 <dejmek.tomas@email.cz>
2025-04-22 10:24:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Ahoj ze světa Azure!"
+def index():
+    return render_template('index.html')
+
+@app.route('/action', methods=['POST'])
+def action():
+    button = request.form['button']
+    return f"Stiskl jsi: {button}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
